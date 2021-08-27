b5aab629c010ebc16210869bb36d7dcb4e5d64e8
Peter <peter.gallwas@outlook.com>
2021-08-28 09:48:41 +1200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/*this is an event */
+/*alert("Im annoying");*/
+
+
+var username = prompt("Please enter your name");
+var car = new Array("BMW", "Sabb", "Maratia");
+
+if(username==="Husky") {
+
+    document.write("<h1>Hello " + username + " You won " + car[0] + "!</h1>");
+
+}
+else {
+    document.write("<h1>Hello " + username + "!</h1>");
+}
+
