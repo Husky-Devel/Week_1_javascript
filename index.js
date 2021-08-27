d64738e548831540cd9fca865d9774f52da51a53
Peter <peter.gallwas@outlook.com>
2021-08-28 09:53:06 +1200

    sv

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 var username = prompt("Please enter your name");
 var car = new Array("BMW", "Sabb", "Maratia");
 
-if(username==="Husky") {
+if(username==="Husky" , "husky" , "Husky-webdev" , "husky-webdev" , "webdev" ) {
 
     document.write("<h1>Hello " + username + " You won " + car[0] + "!</h1>");
 
