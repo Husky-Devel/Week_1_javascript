48c8f4bf6c405716259baa72f7b3104908f13158
Peter <peter.gallwas@outlook.com>
2021-09-01 13:06:17 +1200

    sv

--- index.js
+++ index.js
@@ -8,10 +8,20 @@ console.log(username);
 
 if(username==="Husky") {
 
-    document.write("<h1>Hello " + username + " You won " + car[0] + "!</h1>");
+    document.write("<h1>Hello " + username + " You won a " + car[0] + "!</h1>");
 
 }
-else {
-    document.write("<h1>Hello " + username + "!</h1>");
+else if (username==="Husky-webdev") {
+    document.write("<h1>Hello " + username + " You won a " + car[2] + "!</h1>");
+
 }
+
+else if (username==="preston") {
+    document.write("<h1>Hello " + username + " You won a " + car[3] + "!</h1>");
+
+}
+
+
+
+/*document.write("<h1>Hello " + username + "!</h1>");*/
 
