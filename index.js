d154c12f7cdbfc43109173ef20457f96eae42664
Peter <peter.gallwas@outlook.com>
2021-09-01 19:09:03 +1200

    sv

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ console.log(username);
 if(username==="Husky") {
 
     document.write("<h1>Hello " + username + " You won a " + car[0] + "!</h1>");
+    document.write("<img src=\"Images/BMW.bmp\" alt=\"Husky-webdev\'s logo\"> ");
 
 }
 else if (username==="Husky-webdev") {
     document.write("<h1>Hello " + username + " You won a " + car[1] + "!</h1>");
+    document.write("<img src=\"Images/sabb.bmp\" alt=\"Husky-webdev\'s logo\"> ");
 
 }
 
 else if (username==="Preston") {
     document.write("<h1>Hello " + username + " You won a " + car[2] + "!</h1>");
+    document.write("<img src=\"Images/maruti%20.bmp\" alt=\"Husky-webdev\'s logo\"> ");
 
 }
 
 else {
     document.write("<h1>Hello " + username + "</h1>");
-    document.write("<img src=\"Images/jlumycg9.bmp\" alt=\"Husky-webdev\'s logo\"> ");
+    document.write("<img src=\"Images/jlumycg9.bmp\" alt=\"Husky-webdev\'s logo\" height=\'200px\' width=\'200px\'> ");
 
 }
 
